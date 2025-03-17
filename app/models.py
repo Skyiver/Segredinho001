3ecd7bd27bd62fdf9f09cf7aaea295dd4ccd42e6
MadixSZ <amandascp11@gmail.com>
2025-03-17 19:50:23 -0300

    models_atualizado

--- app/models.py
+++ app/models.py
@@ -1,31 +1,49 @@
 class  Aluno: 
+    id_counter = 1
 
-    def __init__(self, nome, id):
+    def __init__(self, id, nome, idade, turma_id, data_nascimento, 
+                 nota_primeiro_semestre, nota_segundo_semestre, 
+                 media_final):
 
+        self.id = Aluno.id_counter
+        Aluno.id_counter += 1
         self.nome = nome 
-        self.id = id
+        self.turma_id = turma_id
+        self.idade = idade
+        self.data_nascimento = data_nascimento
+        self.nota_primeiro_semestre = nota_primeiro_semestre
+        self.nota_segundo_semestre = nota_segundo_semestre
+        self.media_final = media_final
 
     def to_dict(self):
-        return {"nome": self.nome, "id": self.id}
+        return vars (self)
 
 class Professor:
+    id_counter = 1
 
-    def __init__(self, nome, id):
+    def __init__(self, id, nome, idade, materia, observacao):
 
-        self.nome = nome 
-        self.id = id
+        self.id = Professor.id_counter
+        Professor.id_counter += 1
+        self.nome = nome
+        self.idade = idade
+        self.materia = materia
+        self.observacao = observacao
 
     def to_dict(self):
-        return {"nome": self.nome, "id": self.id}
+        return vars (self)
 
 class Turma:
+    id_counter = 1
 
-    def __init__(self, nome, id, idProfessor):
-
-        self.nome = nome 
-        self.id = id
+    def __init__(self, id, descricao, idProfessor, ativo):
+ 
+        self.id = Turma.id_counter
+        Turma.id_counter += 1
+        self.descricao = descricao
         self.idProfessor = idProfessor
+        self.ativo = ativo
 
     def to_dict(self):
-      return {"nome": self.nome, "id": self.id, "idProfessor": self.idProfessor}
+      return vars (self)
         
